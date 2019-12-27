4ef13aebb2f4db097e69354172b9f6d429f26e36
Marcin Kurka <marcinkurka97@gmail.com>
2019-12-27 17:23:02 +0100

    Netlify deploy bug fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
